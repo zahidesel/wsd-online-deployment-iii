9fdde281ce2a00f852a58763cd8e43f36858647f
zahidesel <zahidenurselcuk@gmail.com>
2023-10-07 21:31:12 +0300

    database_url

--- app/database/database.js
+++ app/database/database.js
@@ -1,13 +1,19 @@
 import postgres from "https://deno.land/x/postgresjs@v3.3.5/mod.js";
 
-const sql = postgres({
- host: "localhost",
+let sql;
+if (Deno.env.get("DATABASE_URL")) {
+  sql = postgres(Deno.env.get("DATABASE_URL"));
+} else {
+  sql = postgres({
+host: "localhost",
   database: "postgres",
   username: "postgres",
   password: "zenebe90",
   port: 5432,
   max: 2, 
+  });
+}
 
-});
+
 
 export { sql };
